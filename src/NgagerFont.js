f885c3b25cc5f17df5647c311ea69d6e3c1c69d4
Long Nguyen <nclong87@gmail.com>
2020-04-02 17:31:11 +0700

    Add NgagerFont component for App

---
+++ src/NgagerFont.js
@@ -0,0 +1,5 @@
+import { createIconSetFromIcoMoon } from 'react-native-vector-icons';
+import ngagerFontConfig from './selection.json';
+
+const NgagerFont = createIconSetFromIcoMoon(ngagerFontConfig);
+export default NgagerFont;
